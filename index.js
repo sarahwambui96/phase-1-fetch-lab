b95bd074fe8595310e3785d155089c92ec78f932
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-24 19:16:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let fetchs = fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (data){
+    renderBooks(data)
+  })
+  return fetchs;
 }
 
 function renderBooks(books) {
